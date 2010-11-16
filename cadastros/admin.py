40ce4ebb21110f96acf1de6a5b7fcd7067ec2228
root <root@adm-gti-m4.(none)>
2010-11-16 16:51:55 -0200

    Foi alterado os campos de telefone e fax

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -1,19 +1,20 @@
 from django import forms
 from models import Fornecedor
 from django.contrib import admin
-from django.contrib.localflavor.br.forms import BRCNPJField,BRPhoneNumberField
+from django.contrib.localflavor.br.forms import BRCNPJField,BRPhoneNumberField,BRStateSelect
 
 class FornecedorForm(forms.ModelForm):
     class Meta:
         model = Fornecedor
     telefone = BRPhoneNumberField()
+    fax = BRPhoneNumberField()
 
 class FornAdmin(admin.ModelAdmin):
     search_fields = ['razao','fantasia','area','obs','cidade','endereco']
-    list_display = ['razao','fantasia','area','telefone','site','data']
+    list_display = ['razao','fantasia','area','telefone','fax','site','data']
     list_filter = ['area']
     date_hierarchy = 'data'
-    list_display_links = ['razao','fantasia']
+    list_display_links = ['razao']
     form = FornecedorForm
 
 
